097ede2c5287e11572681b85eeee5b821f327475
Z3phery <dz.ignacioaguilar@gmail.com>
2025-12-05 21:43:20 -0300

    fix type error (skill issue)

--- kernel/sys.c
+++ kernel/sys.c
@@ -1239,7 +1239,6 @@ static int override_release(char __user *release, size_t len)
 	}
 	return ret;
 }
-
 SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 {
 	struct new_utsname tmp;
@@ -1268,19 +1267,12 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 	if (copy_to_user(name, &tmp, sizeof(tmp)))
 		return -EFAULT;
 
-	return 0;
-}
-	up_read(&uts_sem);
-	if (copy_to_user(name, &tmp, sizeof(tmp)))
-		return -EFAULT;
-
 	if (override_release(name->release, sizeof(name->release)))
 		return -EFAULT;
 	if (override_architecture(name))
 		return -EFAULT;
 	return 0;
 }
-
 #ifdef __ARCH_WANT_SYS_OLD_UNAME
 /*
  * Old cruft
